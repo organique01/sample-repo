943bd1d3330aa96c9d8eedadd3fcf4c722367b20
Anique <anique.vandervlugt@gmail.com>
2021-09-14 13:33:07 +0200

    config changes

---
+++ script.py
@@ -0,0 +1,13 @@
+import sys
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+def greet (who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+//print(greet('World'))
+//print(greet('Me'))
+r = requests.get('https')
